c9009628cbffb335d87b8c585c3d6b5fd614be4a
hiremecenter <contact@hireme.center>
2020-01-16 16:17:12 -0400

    commit

--- src/components/Expertise.vue
+++ src/components/Expertise.vue
@@ -25,12 +25,12 @@ export default {
             expertises:[
                 
                 {
-                    title:"Progracademy",
+                    title:"Progracademy.",
                     description:`Diseñador y programador de la página web para la iniciativa Progracademy: <br/><br/>link: <a class="ed-link" href="http://www.progracademy.org/" target="_blank">progracademy.org</a>`
                 },
                 {
-                    title:"Clases de programacion",
-                    description:`Dicté por unos meses clases de algoritmos y programación de forma gratuita en la casa de la cultura de mi municipio.`
+                    title:"Progracademy.",
+                    description:`Sistema de envío de mensajes por whatsapp utilizando <strong>Golang</strong> como lenguaje base: <br/><br/>link: <a class="ed-link" href="https://github.com/desarrolloenprg/app-whatsapp" target="_blank">app-whatsapp</a>`
                 },
             ],
         }
