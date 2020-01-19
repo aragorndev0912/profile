ab8eaaeed42fc1898be98078871462e57ff4d9ce
Aragorn <aragorn.dev0912@gmail.com>
2020-01-19 00:48:41 -0400

    add ray2d

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -33,10 +33,12 @@ export default {
                     id:0,
                     title:'Amaia virtual machine',
                     subtitle:'Rust (artículo)',
-                    description:`<strong>Amaia virtual machine (AVM)</strong>, es un intérprete para el lenguaje de programación Amaia (en desarrollo), 
-                    el cual no es mas que una simplificación del lenguaje programación Basic. AVM lee ficheros con extensión 
-                    <string>.avm</string>, el contenido del fichero esta definido por una series de números hexadecimales, los cuales poseen 
-                    las instrucciones a ser ejecutadas por la <strong>AVM.</strong>`,
+                    description:`<strong>Amaia virtual machine (AVM)</strong>, es una pequeña maquina virtual que ejecuta instrucciones en números
+                    hexadecimales, la idea era desarrollar un compilador para un lenguaje de programación inventado, en este caso <strong>el lenguaje Amaia</strong>.
+                    El compilador generaría instrucciones hexadecimales como código objeto y esta sería ejecutadas por la <strong>AVM</strong>.<br/><br/>
+                    
+                    La máquina virtual fue programada en Rust, aquí dejo los enlaces a la publicación hecha en linkedin y al codigo fuente.
+                    `,
                     uri:[{title:'Github', uri:'https://github.com/aragorndev0912/amaia-vm'}, {title:'Artículo en Linkedin', uri:'https://www.linkedin.com/pulse/tiny-virtual-machine-franklin-jose-morales-marquez/'}],
                     images:['images/amaia.jpg'],
                 },
@@ -44,13 +46,24 @@ export default {
                     id:1,
                     title:'RPN para Amaia-lang.',
                     subtitle:'Rust (artículo)',
-                    description:`El intérprete del lenguaje de programación Amaia (próximamente se explicará), internamente utilizará la Notación 
+                    description:`El intérprete del lenguaje de programación Amaia (en algún momento me pondré a programarlo), internamente utilizará la Notación 
                     Polaca Inversa. para la evaluación de las expresiones aritméticas. Se desarrolló un módulo escrito en el lenguaje Rust que permite 
                     evaluar expresiones definidas dentro de un fichero de texto con extensión .ope, escritas utilizando la Notación Algebraica, 
                     para luego ser convertidas a expresiones definidas bajo la Notación Polaca Inversa y al final evaluarlas y obtener el resultado 
                     de la operaciones.`,
                     uri:[{title:'Github', uri:'https://github.com/aragorndev0912/rpn'}, {title:'Artículo en Linkedin', uri:'https://www.linkedin.com/pulse/evaluaci%C3%B3n-aritm%C3%A9tica-para-un-compilador-o-int%C3%A9rprete-franklin-jose/'}],
                     images:['images/rpn.jpg'],
+                },
+                {
+                    id:2,
+                    title:'Overflow Engine.',
+                    subtitle:'ANSI C',
+                    description:`Ray2D (Overflow Engine) es un editor de mapas que estoy desarrollando para la creación de un videojuego similar al <strong>Wolfenstein 3D</strong>, lo estoy
+                    programando utilizando una librería de C bastante simple para el prototipado y la programación de videojuegos, Raylib.
+                    Tengo este proyecto algo abandonado porque ahorita estoy con mi intérprete <a class="about-link" href="https://github.com/aragorndev0912/beverly-lang" target="_blank">Beverly</a>, 
+                    una vez lo acabe, retomaré este proyecto.`,
+                    uri:[{title:'Github Ray2D', uri:'https://github.com/aragorndev0912/Ray2D'}],
+                    images:['images/ray2d_0.jpg', 'images/ray2d_1.jpg'],
                 },
             ],
         }
